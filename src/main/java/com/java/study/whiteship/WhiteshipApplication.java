c09b607b897c615438b303958fa46878ee49bf50
qwe5507 <apvmffkdls@naver.com>
2022-04-22 17:44:00 +0900

    Initial commit

---
+++ src/main/java/com/java/study/whiteship/WhiteshipApplication.java
@@ -0,0 +1,13 @@
+package com.java.study.whiteship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/study/whiteship/WhiteshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.study.whiteship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
